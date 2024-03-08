47f6a44e24eb2303e0d0bf385a1f91b7b2dfe0c5
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2024-03-09 00:30:00 +0900

    Re-implement the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sankichi.net",
+  integrations: [mdx(), sitemap()],
+});
